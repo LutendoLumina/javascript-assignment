518b8b26550a55a61165360ae86cabd9b46cb3ef
Lutendo Matshidze <Lupreshie@gmail.com>
2026-04-19 14:51:52 +0200

    Updated greetBot exercise

--- greetBot.js
+++ greetBot.js
@@ -1,16 +1,11 @@
-function greetingBot (name, isMorning) {
-    if (isMorning === true) {
-        return `Good morning ${name}!`
-    }
-    else {
-        return `Hello, ${name}!`
-    }
+function greetingBot(name, isMorning) {
+  return isMorning ? `Good morning ${name}!` : `Hello, ${name}!`;
 }
 
-let greet1 = greetingBot('Lumina', false);
-let greet2 = greetingBot('Joe', true);
-let greet3 = greetingBot('Zara', false);
-let greet4 = greetingBot('Keisha', true);
+let greet1 = greetingBot("Lumina", false);
+let greet2 = greetingBot("Joe", true);
+let greet3 = greetingBot("Zara", false);
+let greet4 = greetingBot("Keisha", true);
 
 console.log(greet1);
 console.log(greet2);
